2dae575d2277468317126bfe8b62be760dc5c5b2
Soldy <4786022+Soldy@users.noreply.github.com>
2021-02-07 18:55:07 +0000

    multi examples
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- example.js
+++ example.js
@@ -4,6 +4,48 @@ let stylerc  = new (require('./index.js')).base();
 
 
 console.log(
-    stylerc.style('test',{color:'red',background:'white',effect:'blink'})
+    stylerc.style(
+        'test red text',
+        {
+             'color':'red'
+        }
+    )
 );
+
+console.log(
+    stylerc.style(
+        'test text with blue background',
+        {
+             'background':'blue'
+        }
+    )
+);
+
+// simple effect
+console.log(
+    stylerc.style(
+        'blinking  test text',
+        {
+             'effect':'blink'
+        }
+    )
+);
+
+
+//simple multiple style 
+console.log(
+    stylerc.style(
+        'styled test text',
+        {
+             'color':'red',
+             'background':'cyan',
+             'effect':[
+                'blink',
+                'underline'
+             ]
+        }
+    )
+);
+
+
 
